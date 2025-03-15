e561fef69c318452b8659f7d97c8f95f3ef88de6
andric-dusica <dusica.andric@gmail.com>
2025-03-15 02:18:36 +0100

    fixing err

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,17 @@ module.exports = {
     "./our-work/**/*.html",
     "./js/**/*.js",
   ], 
+  safelist: [
+    'grid-cols-1',
+    'grid-cols-2',
+    'grid-cols-3',
+    'grid-cols-4',
+    'gap-[24px]',
+    'lg:grid-cols-3',
+    'xl:grid-cols-4',
+    'mx-auto',
+    // sve što dodaš iz JS-a
+  ],
   theme: {
     extend: {
       colors: {
