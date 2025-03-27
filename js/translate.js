5281a22c05034ab03f648ca07bbc6a4dbef7c972
andric-dusica <dusica.andric@gmail.com>
2025-03-27 16:52:06 +0100

    small fix

--- js/translate.js
+++ js/translate.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     const lang = localStorage.getItem("lang") || "en";
     
     try {
-      const response = await fetch(`locales/${lang}.json`);
+      const response = await fetch(`/locales/${lang}.json`);
       const translations = await response.json();
     
       document.querySelectorAll("[data-i18n]").forEach((el) => {
