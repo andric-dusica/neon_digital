f8107fa9188dc6e28a3d7e6b40aded5673fb3662
andric-dusica <dusica.andric@gmail.com>
2025-11-08 12:12:56 +0100

    chore: clean vite config and remove webpack dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,26 @@
-import { defineConfig } from 'vite';
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
+// vite.config.js
+import { defineConfig } from "vite";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
   server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5173',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+    open: true, // automatski otvara browser
   },
   css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "./src/styles/variables.scss";`,
-      },
-    },
     postcss: {
-      plugins: [tailwindcss, autoprefixer],
+      plugins: [tailwindcss(), autoprefixer()],
     },
-  },
-  optimizeDeps: {
-    include: ['@fancyapps/ui'],
   },
   build: {
     rollupOptions: {
       input: {
-        main: 'index.html',
-        services: 'services/index.html',
-        aboutUs: 'about-us/index.html',
-        ourWork: 'our-work/index.html',
-        contactUs: 'contact-us/index.html',
-      }
-    }
+        main: "index.html",
+        services: "services/index.html",
+        aboutUs: "about-us/index.html",
+        ourWork: "our-work/index.html",
+        contactUs: "contact-us/index.html",
+      },
+    },
   },
 });
